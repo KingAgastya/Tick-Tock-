484707c45b682c6b67f62238e4053a4ec8f1f49e
KingAgastya <63898735+KingAgastya@users.noreply.github.com>
2021-02-26 22:08:03 +0530

    Added Tweaks to Sketch.js
    This is now free of bugs :)

--- Tick Tock/sketch.js
+++ Tick Tock/sketch.js
@@ -2,43 +2,55 @@ var hr, min, sec;
 var hourHand, minHand, secHand; 
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(400,400); 
   angleMode(DEGREES)
-  translate(400,200)
 }
 
 function draw() {
-  background(255,255,255);
+  background(255, 255, 255);
+
+  translate(200, 200)
+  rotate(-90)
+
   hr = hour();
   min = minute();
   sec = second();
 
+  
+  hourHand = map(hr % 12, 0, 12, 0, 360)
+  minHand = map(min, 0, 60, 0, 360)
+  secHand = map(sec, 0, 60, 0, 360);
+
+
   push();
   rotate(secHand);
-  strokeWeight(5)
+  strokeWeight(7)
+  stroke("yellow")
   line(0, 0, 100, 0)
   pop();
 
   push();
   rotate(minHand)
-  strokeWeight(8)
-  line(0, 0, 100, 0)
+  stroke("cyan")
+  strokeWeight(7)
+  line(0, 0, 75, 0)
   pop();
 
   push();
   rotate(hourHand)
-  strokeWeight(10)
-  line(0, 0, 100, 0)
+  strokeWeight(7)
+  stroke("lime")
+  line(0, 0, 50, 0)
   pop();
 
-  stroke(255, 20, 150);
-  strokeWeight(5)
+  stroke("red");
+  strokeWeight(10)
   noFill();
   stroke("yellow");
-  arc(0, 0, 300, 300, 0, hourHand)
+  arc(0, 0, 300, 300, 0, secHand)
   stroke("cyan");
-  arc(0, 0, 270, 270, 0, minHand)
+  arc(0, 0, 280, 280, 0, minHand)
   stroke("lime");
-  arc(0, 0, 250, 250, 0, secHand)
+  arc(0, 0, 260, 260, 0, hourHand)
   drawSprites();
 }
